dfbbaf6489cea8b35a81b3db7a4e07cec1724390
Antonio Fabregat <fabregat.antonio@gmail.com>
2014-11-05 17:23:50 +0000

    about field is now a list of strings

--- packages/java/input-data-format/src/main/java/org/cttv/input/model/BiologicalObject.java
+++ packages/java/input-data-format/src/main/java/org/cttv/input/model/BiologicalObject.java
@@ -3,21 +3,29 @@ package org.cttv.input.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @author Antonio Fabregat <fabregat@ebi.ac.uk>
  */
 @JsonInclude(Include.NON_NULL)
 public class BiologicalObject {
 
-    private String about;
+    private List<String> about;
 
     private BiologicalObjectProperties properties;
 
     public BiologicalObject(String about){
-        this.about = about;
+        this.about = new LinkedList<String>();
+        this.about.add(about);
     }
 
-    public String getAbout() {
+    public boolean addAbout(String about){
+        return this.about.add(about);
+    }
+
+    public List<String> getAbout() {
         return about;
     }
 

47c83857ee95ab7ffb13767d3c1aacf17b478647
Maitesin <violador.de.segmentos@gmail.com>
2014-11-12 11:09:59 +0000

    Tests for LinkOut and ProvenanceUrls

---
+++ packages/java/input-data-format/src/test/java/org/cttv/input/model/LinkOutTest.java
@@ -0,0 +1,18 @@
+package org.cttv.input.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LinkOutTest {
+
+    @Test
+    public void testGetNiceName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetUrl() throws Exception {
+
+    }
+}
---
+++ packages/java/input-data-format/src/test/java/org/cttv/input/model/ProvenanceUrlsTest.java
@@ -0,0 +1,28 @@
+package org.cttv.input.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProvenanceUrlsTest {
+
+    @Test
+    public void testSetBiojs() throws Exception {
+
+    }
+
+    @Test
+    public void testAddLinkOut() throws Exception {
+
+    }
+
+    @Test
+    public void testGetBiojs() throws Exception {
+
+    }
+
+    @Test
+    public void testGetLinkouts() throws Exception {
+
+    }
+}

1f8170f889e1a5c7196232ceedd91a0e4bf3413b
Edward Turner <eddturner@ebi.ac.uk>
2014-11-13 09:32:45 +0000

    couple of typos + duplicate value

--- packages/java/input-data-format/src/main/java/org/cttv/input/model/Activity.java
+++ packages/java/input-data-format/src/main/java/org/cttv/input/model/Activity.java
@@ -7,10 +7,9 @@ public enum Activity {
     UP ("http://identifiers.org/cttv.activity/up"),
     DOWN ("http://identifiers.org/cttv.activity/down"),
     UNKNOWN ("http://identifiers.org/cttv.activity/unknown"),
-    DECRESED_TRANSCRIPT_LEVEL ("http://identifiers.org/cttv.activity/decreased_transcript_level"),
-    INCRESED_TRANSCRIPT_LEVEL ("http://identifiers.org/cttv.activity/increased_transcript_level"),
-    DECRESED_TRANSLATIONAL_PRRODUCT_LEVEL ("http://identifiers.org/cttv.activity/decreased_translational_product_level"),
-    INCREASED_TRANSCRITP_LEVEL ("http://identifiers.org/cttv.activity/increased_transcript_level"),
+    DECREASED_TRANSCRIPT_LEVEL ("http://identifiers.org/cttv.activity/decreased_transcript_level"),
+    DECREASED_TRANSLATIONAL_PRODUCT_LEVEL ("http://identifiers.org/cttv.activity/decreased_translational_product_level"),
+    INCREASED_TRANSCRIPT_LEVEL ("http://identifiers.org/cttv.activity/increased_transcript_level"),
     LOSS_OF_FUNCTION ("http://identifiers.org/cttv.activity/loss_of_function"),
     PARTIAL_LOSS_OF_FUNCTION ("http://identifiers.org/cttv.activity/partial_loss_of_function"),
     GAIN_OF_FUNCTION ("http://identifiers.org/cttv.activity/gain_of_function");

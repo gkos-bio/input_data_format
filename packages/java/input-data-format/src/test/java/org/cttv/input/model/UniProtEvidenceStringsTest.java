133614756b31a653ad51499640131a35e5dd508a
Edward Turner <eddturner@ebi.ac.uk>
2014-11-13 09:45:54 +0000

    pom with logback dep; simple 'sanity' test for uniprot added. exercising use of new builders + generating json.

---
+++ packages/java/input-data-format/src/test/java/org/cttv/input/model/UniProtEvidenceStringsTest.java
@@ -0,0 +1,73 @@
+package org.cttv.input.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+
+/**
+ * Created by eddturner on 13/11/14.
+ */
+public class UniProtEvidenceStringsTest {
+    // logger
+    private static final Logger logger = LoggerFactory.getLogger(UniProtEvidenceStringsTest.class);
+
+    /**
+     * test whether some valid json can be produced
+     */
+    @Test
+    public void simpleJsonGenerationTest() {
+        ObjectMapper mapper = new ObjectMapper();
+
+        EvidenceString simpleUniProtEString = new EvidenceString(null, null, null);
+
+        try {
+            String jsonStr = mapper.writeValueAsString(simpleUniProtEString);
+            logger.info(jsonStr);
+            assertNotNull(jsonStr);
+            assertTrue(jsonStr.startsWith("{"));
+            assertTrue(jsonStr.endsWith("}"));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * sanity test. checking whether some "about" attribute in a BiologicalSubject
+     * instance exists
+     */
+    @Test
+    public void containsBioSubjTest() {
+        ObjectMapper mapper = new ObjectMapper();
+
+        EvidenceString simpleUniProtEString = EvidenceString.newBuilder().withBiologicalSubject(
+                BiologicalSubject
+                        .newBuilder()
+                        .withAbout(
+                                new ArrayList<String>() {
+                                    {
+                                        add("about1");
+                                    }
+                                }).build())
+                .build();
+
+        try {
+            String jsonStr = mapper.writeValueAsString(simpleUniProtEString);
+            logger.info(jsonStr);
+            assertNotNull(jsonStr);
+            assertTrue(jsonStr.startsWith("{"));
+            assertTrue(jsonStr.endsWith("}"));
+            assertTrue(jsonStr.contains("about"));
+
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+}

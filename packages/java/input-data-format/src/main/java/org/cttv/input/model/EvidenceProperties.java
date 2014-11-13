157f2a4d0a6eb86e3bf2a113414a51e953135fa3
Antonio Fabregat <fabregat.antonio@gmail.com>
2014-11-13 14:50:07 +0000

    contains the properties for evidence objects

---
+++ packages/java/input-data-format/src/main/java/org/cttv/input/model/EvidenceProperties.java
@@ -0,0 +1,37 @@
+package org.cttv.input.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+
+import java.util.List;
+
+/**
+ * @author Antonio Fabregat <fabregat@ebi.ac.uk>
+ */
+public class EvidenceProperties {
+    private List<EvidenceString> chain;
+    private ExperimentSpecific experimentSpecific;
+
+    public EvidenceProperties() {
+        //Nothing here
+    }
+
+    @JsonProperty(value = "evidence_chain", required = false)
+    public List<EvidenceString> getChain() {
+        return chain;
+    }
+
+    @JsonProperty(value = "experiment_specific", required = false)
+    @JsonPropertyDescription("Use this to add any string-based key:value pairs that you want to use to describe your biological object.")
+    public ExperimentSpecific getExperimentSpecific() {
+        return experimentSpecific;
+    }
+
+    public void setChain(List<EvidenceString> chain) {
+        this.chain = chain;
+    }
+
+    public void setExperimentSpecific(ExperimentSpecific experimentSpecific) {
+        this.experimentSpecific = experimentSpecific;
+    }
+}

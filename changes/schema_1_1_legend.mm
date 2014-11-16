d0b4723dd7395ccd26960653af491c1fee59bb04
Samiul Hasan <shasan77@gmail.com>
2014-11-16 19:34:41 +0000

    first commit

---
+++ changes/schema_1_1_legend.mm
@@ -0,0 +1,28 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#ffcc33" CREATED="1415706426564" ID="ID_58045614" MODIFIED="1416166194609" STYLE="bubble" TEXT="Legend">
+<edge COLOR="#ffffff"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node BACKGROUND_COLOR="#ffcc33" CREATED="1415710313586" HGAP="153" ID="ID_1736177401" MODIFIED="1416166135378" POSITION="right" TEXT="Association between a biological subject and a biological object" VSHIFT="43">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node BACKGROUND_COLOR="#33f8ff" CREATED="1415706439507" HGAP="155" ID="ID_379658927" MODIFIED="1416166144228" POSITION="right" STYLE="bubble" TEXT="biological_subject">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node BACKGROUND_COLOR="#ff9999" CREATED="1415706476189" HGAP="155" ID="ID_1166247508" MODIFIED="1416166147957" POSITION="right" STYLE="bubble" TEXT="biological_object" VSHIFT="2">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1416165965141" HGAP="156" ID="ID_1382380810" MODIFIED="1416166149782" POSITION="right" TEXT="target (gene, protein, protein complex etc.)" VSHIFT="-1">
+<icon BUILTIN="target"/>
+</node>
+<node CREATED="1416165988927" HGAP="156" ID="ID_1074915897" MODIFIED="1416166152025" POSITION="right" TEXT="evidence" VSHIFT="-2">
+<icon BUILTIN="evidence"/>
+</node>
+<node CREATED="1416166005050" HGAP="153" ID="ID_767977472" MODIFIED="1416166154151" POSITION="right" TEXT="disease" VSHIFT="-3">
+<icon BUILTIN="disease"/>
+</node>
+<node CREATED="1416166015018" HGAP="152" ID="ID_819859765" MODIFIED="1416166157115" POSITION="right" TEXT="SNP" VSHIFT="-94">
+<icon BUILTIN="snp"/>
+</node>
+</node>
+</map>

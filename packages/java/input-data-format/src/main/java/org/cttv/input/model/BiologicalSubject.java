c8aac5389f5263bf748ac253445790e22b8fb1c4
Antonio Fabregat <fabregat.antonio@gmail.com>
2014-11-19 11:20:20 +0000

    about changed to match with the requiremens (not a list with "[" and "]" but values separated by ",")

--- packages/java/input-data-format/src/main/java/org/cttv/input/model/BiologicalSubject.java
+++ packages/java/input-data-format/src/main/java/org/cttv/input/model/BiologicalSubject.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.sun.deploy.util.StringUtils;
 
 import java.util.List;
 
@@ -24,8 +25,8 @@ public class BiologicalSubject {
 
     @JsonProperty(value = "about", required = true)
     @JsonPropertyDescription("An array of identifier.org URIs. For example, if depicting a CTTV target, which is a heteropolymeric protein complex, use: [http://identifiers.org/uniprot/P251200, http://identifiers.org/uniprot/P35348, http://identifiers.org/uniprot/P35368]")
-    public List<String> getAbout() {
-        return about;
+    public String getAbout() {
+        return StringUtils.join(about, ",");
     }
 
     @JsonProperty(value = "properties", required = false)
